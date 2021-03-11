748ba59fef239a2e91512088feadf8d1f35db5a1
monomilitis <74012189+monomilitis@users.noreply.github.com>
2021-03-12 00:34:15 +0300

    Add files via upload

---
+++ свой проект/js/main.js
@@ -0,0 +1,7 @@
+$('.slider_inner').slick({
+    infinite: false,
+    touchMove: false,
+    swipe: false,
+    nextArrow: '<button type="button" class="slick_btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick_btn slick-prev"></button>',
+});
